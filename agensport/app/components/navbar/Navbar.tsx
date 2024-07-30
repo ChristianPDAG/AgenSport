8b2e2074bfb8b980cc06056d875947a543ca9423
ChristianPDAG <christian.perezdearce@gmail.com>
2024-07-29 22:13:52 -0400

    prueba

---
+++ agensport/app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <nav className='w-full bg-black'>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
