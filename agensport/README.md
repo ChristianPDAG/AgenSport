cbb47eb1af0866c225fc544c16c5bd89034734c9
ChristianPDAG <christian.perezdearce@gmail.com>
2024-07-30 20:25:44 -0400

    estilo añadido a global.css y readme configurado

---
+++ agensport/README.md
@@ -0,0 +1,3 @@
+.custom-gradient -> es el codigo para el gradiente
+verde-oscuro:(background) y verde-claro(hover) -> componentes de colores principales utilizados
+##una buena practica es agregar a 'global.css' las combinaciones de tailwind que se repiten por ejemplo .menu_btn##
--- agensport/app/components/navbar/MenuLink.tsx
+++ agensport/app/components/navbar/MenuLink.tsx
@@ -10,10 +10,10 @@ const MenuLink = () => {
       <div className="sm:hidden flex">
 
       <div className="absolute top-[50px] left-0 right-0 w-full bg-verde-oscuro opacity-90 border-black rounded-xl shadow-md flex flex-col cursor-pointer">
-            <button className="w-full border-b py-1.5 px-5 text-white transition-all hover:bg-verde-claro hover:text-verde-oscuro text-center text-xs font-bold font-sans flex items-center justify-center rounded-xl">
+            <button className="menu_btn">
             Iniciar sesión
             </button>
-            <button className="w-full border-b  py-1.5 px-5 text-white transition-all hover:bg-verde-claro hover:text-verde-oscuro text-center text-xs font-bold font-sans flex items-center justify-center rounded-xl">
+            <button className="menu_btn">
             Registrarse
             </button>
             {/* Puedes agregar más botones aquí */}
